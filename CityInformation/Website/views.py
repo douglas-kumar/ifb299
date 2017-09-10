63482d63c085860b98eae64e8bdea41c054d5c08
15caloke <calum.oke@connect.qut.edu.au>
2017-09-10 16:26:34 +1000

    Webpage Viewable
    First Webpage using Simon's Web Design can now be seen using the URL:
    http://127.0.0.1:8000/Website/ (assuming your server is running). Still
    needs obvious improvement

--- CityInformation/Website/views.py
+++ CityInformation/Website/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Home Page
+def index(request):
+    return HttpResponse("""<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="title">
+			<img src="http://www.adstockglobal.com/wp-content/uploads/2016/02/City-Info.png">
+			</div>
+			<div id="regologin">
+				<input type="submit" value="Register">
+				<input type="submit" value="Login">
+			</div>
+			<div id="main">
+			I AM THE MAIN THINGO
+			</div>
+		</div>
+	
+	
+	</body>
+
+
+</html>
+
+ """)
