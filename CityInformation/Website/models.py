4cb51cd78a6ac34ee1239314b59178ea6869ac8f
Luke8 <30490006+Luke8@users.noreply.github.com>
2017-10-01 14:23:06 +1000

    Added datasets of all info categories + Event + PublicTrans info
    Some modifications may be required. Let me know if you noticed anything. Thanks

--- CityInformation/Website/models.py
+++ CityInformation/Website/models.py
@@ -31,4 +31,89 @@ class Library(models.Model):
     def __str__(self):
         return self.name
 
+class Event(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
+
+class PublicTrans(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
+
+class Industries(models.Model):
+    name = models.CharField(max_length=250)
+    address = models.CharField(max_length=500)
+    industryType = models.CharField(max_length=500)
+    email = models.CharField(max_length=250, null=True)
+    image = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name + ' - ' + self.address
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=250)
+    address = models.CharField(max_length=500)
+    phone = models.CharField(max_length=250)
+    email = models.CharField(max_length=250, null=True)
+    image = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name + ' - ' + self.address
+
+class Park(models.Model):
+    name = models.CharField(max_length=250)
+    address = models.CharField(max_length=500)
+    phone = models.CharField(max_length=250)
+    email = models.CharField(max_length=250, null=True)
+    image = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name + ' - ' + self.address
+
+class Zoo(models.Model):
+    name = models.CharField(max_length=250)
+    address = models.CharField(max_length=500)
+    phone = models.CharField(max_length=250)
+    email = models.CharField(max_length=250, null=True)
+    image = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name + ' - ' + self.address
+
+class Museum(models.Model):
+    name = models.CharField(max_length=250)
+    address = models.CharField(max_length=500)
+    phone = models.CharField(max_length=250)
+    email = models.CharField(max_length=250, null=True)
+    image = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name + ' - ' + self.address
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=250)
+    address = models.CharField(max_length=500)
+    phone = models.CharField(max_length=250)
+    email = models.CharField(max_length=250, null=True)
+    image = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name + ' - ' + self.address
+
+class Mall(models.Model):
+    name = models.CharField(max_length=250)
+    address = models.CharField(max_length=500)
+    phone = models.CharField(max_length=250)
+    email = models.CharField(max_length=250, null=True)
+    image = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name + ' - ' + self.address
+
+
 
