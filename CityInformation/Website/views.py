753f3a735cf88932a341a7105062d5011c5b9408
Will <william.atkinson@connect.qut.edu.au>
2017-10-06 16:46:16 +1000

    Fixed Login and Logout Pages

--- CityInformation/Website/views.py
+++ CityInformation/Website/views.py
@@ -5,11 +5,11 @@ from .models import College
 from .models import City
 from .models import Profile
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.views.generic import View
 from django.views import generic
-from .forms import UserForm
+from .forms import UserForm, LoginForm
 
 
 class IndexView(generic.ListView):
@@ -68,9 +68,37 @@ class UserFormView(View):
             user = authenticate(username=username, password=password)
 
             if user is not None:
-
                 if user.is_active:
                     login(request, user)
                     return redirect('/Website/')
 
         return render(request, self.template_name, {'form': form})
+
+class LoginFormView(View):
+    form_class = LoginForm
+    template_name = 'registration/login.html'
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form' : form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('/Website/')
+
+        return render(request, self.template_name, {'form': form})
+
+def LogOut(request):
+    logout(request)
+    template_name = 'registration/logged_out.html'
+    return render(request, template_name)
