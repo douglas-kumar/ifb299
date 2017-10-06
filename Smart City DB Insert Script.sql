c5ac253a0fba54e67dc1db34d66986e243258822
Will <william.atkinson@connect.qut.edu.au>
2017-10-06 13:02:47 +1000

    Uploaded DB Insert Script

---
+++ Smart City DB Insert Script.sql
@@ -0,0 +1,47 @@
+INSERT INTO user VALUES (1, "admin", "admin@example.com", "Passw0rd1", 4);
+INSERT INTO user VALUES (2, "user1", "user1@gmail.com", "IamThebest", 3);
+INSERT INTO user VALUES (3, "big_lez", "thebiglezshow@gmail.com", "BigLez123", 1);
+INSERT INTO city VALUES (1, "Brisbane", "Queensland", 27.46, 153.02);
+INSERT INTO city VALUES (2, "Sydney", "New South Wales", 33.87, 151.21);
+INSERT INTO city VALUES (3, "Melbourne", "Victoria", 37.81, 144.96);
+INSERT INTO city VALUES (4, "Hobart", "Tasmania", 42.85, 147.29);
+INSERT INTO city VALUES (5, "Adelaide", "South Australia", 34.93, 138.6);
+INSERT INTO city VALUES (6, "Perth", "Western Australia", 31.96, 115.84);
+INSERT INTO city VALUES (7, "Darwin", "Northern Territory", 12.43, 130.85);
+INSERT INTO city VALUES (8, "Canberra", "New South Wales", 35.31, 149.13);
+INSERT INTO publicTrans VALUES(1, 1, "Translink Bus", "Bus System around Brisbane area");
+INSERT INTO event VALUES(200, 1, "Ultra Music Festival 2020", "Only the Best Music from the Best Artists");
+INSERT INTO industries VALUES(345, 5, "Adelaide's Coal Industry", "1 Main St, Adelaide City", "Mining", 
+								"wedealwithcoal@gmail.com", 
+                                "https://upload.wikimedia.org/wikipedia/commons/b/b3/Strip_coal_mining.jpg");
+INSERT INTO mall VALUES(666, 1, "Westfield Chermside", "Cnr Gympie & Hamilton Rds, Chermside", "(07) 3117 5300"
+								"chermside@scentregroup.com", "https://findnearest.com/images/logos/westfield_chermside.gif");
+INSERT INTO park VALUES(27, 1, "Kidspace", "Murphy Rd, Chermside", "(07) 3403 8888", "No email", 
+		"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFoYfnKiXsV7wt3lB7joaP9g6BBaeMd_Hqmr5lnRD9lEqA4N6g");
+INSERT INTO restaurant VALUES(1, 1, "Verve Cafe", "109 Edward St, Brisbane City", "(07) 3221 5691", 
+								"info@vervecafe.com.au", 
+								"https://static.wixstatic.com/media/d602ac_ae4e4f76f1854ff99de2f16c642360c1~mv2.png
+                                /v1/crop/x_0,y_0,w_125,h_85/fill/w_117,h_80,al_c,usm_0.66_1.00_0.01/
+                                d602ac_ae4e4f76f1854ff99de2f16c642360c1~mv2.png");
+INSERT INTO restaurant VALUES(3, 1, "Alchemy Restaurant & Bar", "175 Eagle St, Brisbane City", "(07) 3229 3175", 
+								"mail@alchemyrestaurant.com.au", "alchemyrestaurant.com.au/img/alchemy-logo.png");
+INSERT INTO museum VALUES(10, 1, "Museum of Brisbane", "192 Ann St, Brisbane City", "(07) 3339 0800", 
+								"info@museumofbrisbane.com.au", 
+	"http://www.visitbrisbane.com.au/~/media/brisbane-marketing/products/atdw/2017/05/08/14/18/
+			museum-of-brisbane-57ff1cc377af1f7520ac1445-16x9.ashx?bc=black&h=405&w=720");
+INSERT INTO library VALUES(1, 1, "QUT Gardens Point Library", "2 George St, Brisbane City", 
+								 "(07) 3138 2000", "askqut@qut.edu.au", 
+                                 "pbs.twimg.com/profile_images/477262458365554688/oHsHol3V.png");
+INSERT INTO library VALUES(2, 1, "Brisbane Square Library", "266 George St, Brisbane City", 
+								 "(07) 3403 4166", "bsql@brisbane.qld.gov.au", 
+								 "brisbanekids.com.au/wp-content/uploads/2015/11/021-703x468.jpg");
+INSERT INTO zoo VALUES(1, 1, "Australia Zoo", "1638 Steve Irwin Way, Beerwah", "(07) 5436 2000", 
+							 "australiazoo.com.au/contact.php",                              
+"https://upload.wikimedia.org/wikipedia/en/thumb/5/5f/Australia_Zoo_Logo.svg/1123px-Australia_Zoo_Logo.svg.png");
+INSERT INTO hotel VALUES(100, 1, "Sebel Quay West", "132 Alice Street, Brisbane City", "1800 672 726", "H8783@accor.com", 
+								 "https://www.ahstatic.com/photos/8783_ho_00_p_2048x1536.jpg");
+INSERT INTO college VALUES (1, "Queensland University of Technology", "2 George St, Brisbane City", 
+								"SEF, Business, Creative Industries", "askqut@qut.edu.au", 
+                                "qut.edu.au/qut-logo-og-200.jpg");
+INSERT INTO college VALUES (2, "University of Queensland", "St. Lucia", "Science, Engineering", 
+								"uq.edu.au", "uq.edu.au/uqbug/images/NRTWD/UQCrestLogo-A-colour-M.jpg");
