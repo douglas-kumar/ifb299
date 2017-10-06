27bda88a3afee092460e68731d1f3d06b2953d24
qutsimon <simon.william.scott@gmail.com>
2017-10-06 13:16:46 +1000

    Update models.py

--- CityInformation/Website/models.py
+++ CityInformation/Website/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class College(models.Model):
@@ -117,6 +120,17 @@ class Mall(models.Model):
 
     def __str__(self):
         return self.name + ' - ' + self.address
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=250)
 
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
