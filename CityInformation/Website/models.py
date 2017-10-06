ffe970a416f4c43a6c6cfe844880807122bf2947
15caloke <calum.oke@connect.qut.edu.au>
2017-10-06 13:27:21 +1000

    Modified SQL Script to Create DB and New Model
    New model is "User Profile" which will help with registering the user to
    their selected type

--- CityInformation/Website/models.py
+++ CityInformation/Website/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 from django.core.urlresolvers import reverse
 
-# Create your models here.
+# Extend the User model for user types
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    user_type = models.CharField(max_length=20)
+
+    def create_profile(sender, **kwargs):
+        if kwargs['created']:
+            user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+    post_save.connect(create_profile, sender=User)
+
 class College(models.Model):
     name = models.CharField(max_length=250)
     address = models.CharField(max_length=500)
